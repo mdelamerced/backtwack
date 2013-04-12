b9ce4ee368ada0deadb88990c9bcbc7637ff78f2
Melissa <melilssadelamerced@gmail.com>
2013-04-12 01:44:53 -0400

    removed twit in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
-var Twit = require('twit');
+//var Twit = require('twit');
 
 
 
