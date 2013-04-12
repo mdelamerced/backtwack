76557005863af086935d06fa7cf7951e3f1acebc
Melissa <melilssadelamerced@gmail.com>
2013-04-12 01:57:43 -0400

    trying to fix heroku error

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ var T = new Twit({
   , consumer_secret:      process.env.CONSUMER_SECRET
   , access_token:         process.env.ACCESS_TOKEN
   , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
-})
+});
 /*
 	GET /
 */
